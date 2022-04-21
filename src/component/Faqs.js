7609bb3e93f843f77367c899f286ab4ca152f448
jaamasad <jaamasad1@gmail.com>
2022-04-21 16:37:12 +0500

    adding faqs text

--- src/component/Faqs.js
+++ src/component/Faqs.js
@@ -4,11 +4,11 @@ import "../component/asserts/css/faq.css"
 const FAQ = [
   {
     heading: "When is the mint?",
-    text: "PAP holder will be able to join the Training Camp and earn 100% guaranteed daily rewards. PAP holder will be able to acquire new weapons for each of their PAPs and send them off to various missions with unpredictable plots to earn extra rewards and eventually the chance to unlock top-shelf mythical weapons.",
+    text: "May 2022",
   },
   {
     heading: "How much is the mint?",
-    text: "PAP holder will be able to join the Training Camp and earn 100% guaranteed daily rewards. PAP holder will be able to acquire new weapons for each of their PAPs and send them off to various missions with unpredictable plots to earn extra rewards and eventually the chance to unlock top-shelf mythical weapons.",
+    text: "1.5 SOL",
   },
   {
     heading: "What can I do with my PAPs?",
@@ -16,7 +16,7 @@ const FAQ = [
   },
   {
     heading: "Is there a token?",
-    text: "PAP holder will be able to join the Training Camp and earn 100% guaranteed daily rewards. PAP holder will be able to acquire new weapons for each of their PAPs and send them off to various missions with unpredictable plots to earn extra rewards and eventually the chance to unlock top-shelf mythical weapons.",
+    text: "Yes - $BAMBOO is the currency in the world of Post Apocalyptic Pandas. $BAMBOOs are given out as rewards and can be used to acquire weapons and additional gears for increasing individual PAPs’ stats and odds against each missions.",
   },
 ]
 const Faqs = () => {
